3a0791a12b2f18f7da03f2d7e675a5f9dec021f0
Denys iElkin <hvoarang@gmail.com>
2023-01-27 19:31:12 +0200

    js practic

--- src/js/module-one.js
+++ src/js/module-one.js
@@ -39,24 +39,63 @@
 //   const ADMIN_PASSWORD = "";
 //   let i = 0;
 //   for (let i = 0; i >= ADMIN_PASSWORD.length; i++)
-     
+
 // };
 
 // // стек вызовов функций
 
 // const fnA = function () {
-//   console.log("function-1");
-// fnB();
+//   console.log('Total one:');
+//   return fnB();
 // };
-
 // const fnB = function () {
-//   console.log("function-2");
-// fnC();
+//   console.log('Total two:');
+//   return fnC();
 // };
-
 // const fnC = function () {
-//   console.log("function-3");
+//   console.log('Total three:');
+// };
+// fnA();
 
+//  сложение массива
+// const totalPrice = function (items) {
+//   let total = 0;
+//   for (const item of items) {
+//     total += item;
+//   }
+//   return total;
 // };
 
-// fnA();
+// const t1 = totalPrice([21, 34, 123]);
+// const t2 = totalPrice([12, 12, 1223]);
+// const t3 = totalPrice([15, 23, 123]);
+
+// console.log('t1:', t1);
+// console.log('t2:', t2);
+// console.log('t3:', t3);
+
+// функция "name" для перебора и логирования
+
+// const logItems = function (items) {
+//   let total = 0;
+//   for (const item of items) {
+//     console.log(items);
+//   }
+// };
+
+// logItems(['one', 'two', 'three']);
+// logItems([4, 5, 6]);
+// logItems(['seven', 'eght', 'nine']);
+
+// напиши функцию findLogin(allLogins, login) для поиска логина
+//  если логина нет, вывести сообщение "пользователь `${login}` не найден"
+//  если логин найден, вывести сообщение "пользователь `${login}` найден"
+
+const logins = ['123f1f', '12f23f2', 'f132134', '1324f1f3', '12f1f34f'];
+const findLogin = '12f1f34f';
+
+const message = logins.includes(findLogin)
+  ? `Найден ${findLogin}`
+  : `не найден ${findLogin}`;
+
+console.log(message);
