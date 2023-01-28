cb6362f8dcec3ed62e2f5b0da7a9b8636398fef9
hvoarang <90283144+hvoarang@users.noreply.github.com>
2023-01-28 11:04:47 +0000

    Deploying to gh-pages from @ hvoarang/js-only@33024226398c7806433a314ca8bb7ae0f84cd49a 🚀

---
+++ index.9fa2a6f0.js
@@ -0,0 +1,2 @@
+const o=["123@%!@##%%%11","12!@!@#f11","1rf13f4@33r"],n=function(o,n){for(const f of o)if(f===n)return`find ${n}`;return`not find ${n}`};console.log(n(o,"12cvbcvbf11")),console.log(n(o,"12!@!@#f11")),console.log(n(o,"123@%!@##%%%11"));console.log(20);
+//# sourceMappingURL=index.9fa2a6f0.js.map
