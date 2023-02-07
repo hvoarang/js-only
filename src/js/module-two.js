2c3dad66443ffa489308b80fe28ffb46e82a3479
Denys iElkin <hvoarang@gmail.com>
2023-02-07 04:52:06 +0200

    obj

--- src/js/module-two.js
+++ src/js/module-two.js
@@ -1,2 +1,34 @@
-let i = 20;
-console.log(i);
+const playList = {
+  name: 'mine track',
+  rating: 5,
+  tracks: ['track-1', 'track-2', 'track-3', 'track-4'],
+  trackCount: 3,
+  changeName(newName) {
+    console.log('this input NewName', this);
+
+    this.name = newName;
+  },
+  addTrack(track) {
+    this.tracks.push(track);
+  },
+  updateRating(newRating) {
+    this.rating = newRating;
+  },
+  getTackCount() {
+    return this.tracks.length;
+  },
+};
+
+console.log(playList.getTackCount());
+
+playList.changeName('new name');
+
+playList.addTrack('track-5');
+console.log(playList.getTackCount());
+
+playList.addTrack('track-6');
+console.log(playList.getTackCount());
+
+playList.updateRating(6);
+
+console.log(playList);
