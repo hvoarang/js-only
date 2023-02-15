3c0347b7d41990c96ec4d02e888ea9bfc8aec912
Denys iElkin <hvoarang@gmail.com>
2023-02-15 23:50:39 +0200

    practic

--- src/js/module-three.js
+++ src/js/module-three.js
@@ -44,18 +44,154 @@
 // console.log(Math.max(...temp));
 // console.log(Math.min(...temp));
 
-const defoultSettings = {
-  theme: 'light',
-  showNotification: 'true',
-  hideSideBar: 'false',
-};
-const userSettings = {
-  showNotification: 'false',
-  hideSideBar: 'true',
-};
-const finalSettings = {
-  ...defoultSettings,
-  ...userSettings,
-};
+// const defoultSettings = {
+//   theme: 'light',
+//   showNotification: 'true',
+//   hideSideBar: 'false',
+// };
+// const userSettings = {
+//   showNotification: 'false',
+//   hideSideBar: 'true',
+// };
+// const finalSettings = {
+//   ...defoultSettings,
+//   ...userSettings,
+// };
 
-console.log('finalSettings:', finalSettings);
+// console.log('finalSettings:', finalSettings);
+
+// const playlist = {
+//   name: 'My super plailist',
+//   rating: 5,
+//   tracks: ['track-1', 'track-2', 'track-3'],
+//   tracjCount: 3,
+//   avatar: 'https:/google.com',
+//   stats: {
+//     followers: 123,
+//     views: 123,
+//     likes: 123,
+//   },
+// };
+// деструктуризация объекта
+// const { rating, name, tracks, stats, avatar } = playlist;
+
+// console.log(rating, name, tracks, stats, avatar);
+
+// деструктуризация массива
+// const authors = {
+//   kiwi: 3,
+//   poly: 4,
+//   ajax: 5,
+//   mango: 7,
+// };
+
+// const enteries = Object.entries(authors);
+
+// for (const [name, rating] of enteries) {
+//   console.log(name, rating);
+// }
+
+// const playlist = {
+//   name: 'My super plailist',
+//   rating: 5,
+//   tracks: ['track-1', 'track-2', 'track-3'],
+//   tracjCount: 3,
+//   avatar: 'https:/google.com',
+//   stats: {
+//     followers: 123,
+//     views: 123,
+//     likes: 123,
+//   },
+// };
+
+// const { name, rating, avatsr, ...restProps } = playlist;
+// console.log(name, rating, avatsr);
+// console.log(restProps);
+// console.log(playlist);
+
+// патерн функции
+
+// const showPlaylist = function (userPlaylist) {
+//   const {
+//     name,
+//     rating,
+//     tracks,
+//     tracjCount,
+//     avatar,
+//     stats: { followers, views, likes },
+//   } = showPlaylist;
+
+//   console.log(name, rating, tracks, tracjCount, avatar, stats);
+// };
+// showPlaylist(plailist);
+
+// const plailist = {
+//   name: 'My super plailist',
+//   rating: 5,
+//   tracks: ['track-1', 'track-2', 'track-3'],
+//   tracjCount: 3,
+//   avatar: 'https:/google.com',
+//   stats: {
+//     followers: 123,
+//     views: 123,
+//     likes: 123,
+//   },
+// };
+
+// showPlaylist(plailist);
+
+const cart = {
+  items: [],
+  getItems() {
+    return this.items;
+  },
+  add(product) {
+    for (const item of this.items) {
+      if (item.name === product.name) {
+        item.quatity += 1;
+        return;
+      }
+    }
+    const newProduct = {
+      ...product,
+      quatity: 1,
+    };
+    this.items.push(newProduct);
+  },
+  remove(producteName) {
+    const { items } = this;
+
+    for (let i = 0; i < items.length; i += 1) {
+      const { name } = items[i];
+
+      if (producteName === item.names) {
+        console.log('find this product', producteName);
+
+        console.log(i);
+        console.log('asdw');
+        items.splice(i, 1);
+      }
+    }
+  },
+  clear() {},
+  coutTotalPrice() {
+    const { items } = this;
+    let total = 0;
+
+    for (const { price, quatity } of items) {
+      total += price * quatity;
+    }
+    return total;
+  },
+  increaseQuantity(productName) {},
+  decreaseQuantity(productName) {},
+};
+console.log(total);
+console.log(cart.getItems());
+
+cart.add({ name: 'one', prive: 40 });
+cart.add({ name: 'two', prive: 60 });
+cart.add({ name: 'three', prive: 70 });
+cart.add({ name: 'for', prive: 80 });
+
+console.log(cart.getItems());
