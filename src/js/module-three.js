4b2a46c46b47b36d2048d4ad63acd534c3a26dce
Denys iElkin <hvoarang@gmail.com>
2023-02-21 20:54:00 +0200

    fix

--- src/js/module-three.js
+++ src/js/module-three.js
@@ -403,14 +403,31 @@
 
 // user.showUserTag();
 
-// const user = {
-//   tag: 'mango',
-//   showTag() {
-//     console.log('showTag -> this', this.tag);
-//     console.log('showTag -> this', this);
-//   },
-// };
-// user.showTag();
+const counter = {
+  value: 0,
+  increment() {
+    console.log('increment -> this', this);
+    this.value += 1;
+  },
+  decrement() {
+    console.log('decrement -> this', this);
+    this.value -= 1;
+  },
+};
 
-// const outerShowTag = user.showTag;
-// outerShowTag();
+const decrementBtn = document.querySelector('.js-decrement');
+const incrementBtn = document.querySelector('.js-increment');
+const valueEl = document.querySelector('.js-value');
+
+decrementBtn.addEventListener('click', function () {
+  console.log('Increment -');
+
+  counter.document() | console.log(counter);
+  valueEl.textContent = counter.value;
+});
+incrementBtn.addEventListener('click', function () {
+  console.log('Increment +');
+
+  counter.document() | console.log(counter);
+  valueEl.textContent = counter.value;
+});
