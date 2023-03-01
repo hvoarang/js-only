98360c9ec2b8c1eb71b647fe53fe44dc021dcb24
Denys iElkin <hvoarang@gmail.com>
2023-03-01 18:09:01 +0200

    set

--- src/js/module-one.js
+++ src/js/module-one.js
@@ -1,6 +1,18 @@
-console.log('1');
-
-setTimeout(() => {
-  console.log('2');
-}, 2000);
-console.log('3');
+const NOTIFIVATION_DELAY = 3000;
+let = timeoutId = null;
+const refs = {
+  notification: document.querySelector('.js-alert'),
+};
+refs.notification.addEventListener('click', onNotificationClick);
+showNotification();
+function onNotificationClick() {
+  hideNotification();
+  clearInterval(timeoutId);
+}
+function showNotification() {
+  refs.notification.classList.add('is-visible');
+  timeoutId = setTimeout(() => {
+    console.log('close');
+    hideNotification();
+  }, NOTIFIVATION_DELAY);
+}
