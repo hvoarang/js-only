2c498f04dec0b7f050c577694dd09c0ac058649a
Denys iElkin <hvoarang@gmail.com>
2023-03-05 21:30:54 +0200

    fix

--- src/js/module-one.js
+++ src/js/module-one.js
@@ -1,18 +1,14 @@
-const NOTIFIVATION_DELAY = 3000;
-let = timeoutId = null;
-const refs = {
-  notification: document.querySelector('.js-alert'),
-};
-refs.notification.addEventListener('click', onNotificationClick);
-showNotification();
-function onNotificationClick() {
-  hideNotification();
-  clearInterval(timeoutId);
-}
-function showNotification() {
-  refs.notification.classList.add('is-visible');
-  timeoutId = setTimeout(() => {
-    console.log('close');
-    hideNotification();
-  }, NOTIFIVATION_DELAY);
-}
+// итерация массива
+const clients = ['Mango', 'Poly', 'Ajax'];
+//.. итерация начинается с нуля
+console.log(clients[0]);
+console.log(clients[1]);
+//
+//
+// длина массива
+console.log(clients.length);
+//
+//
+// индекс последнего массива
+const lastElementIndex = clients.length - 1;
+console.log(clients[lastElementIndex]); //Ajax
