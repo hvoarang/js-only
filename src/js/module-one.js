1b5486f878e238f16b692a12ba040d5323026f11
Denys iElkin <hvoarang@gmail.com>
2023-03-06 02:19:49 +0200

    fix

--- src/js/module-one.js
+++ src/js/module-one.js
@@ -12,3 +12,12 @@ console.log(clients.length);
 // индекс последнего массива
 const lastElementIndex = clients.length - 1;
 console.log(clients[lastElementIndex]); //Ajax
+console.log(clients.push());
+
+const arr = [1, 2, 3];
+let num = [];
+for (let i = 0; i < arr.length; i += 1) {
+  console.log(arr[i]);
+  num = arr[i].push;
+}
+console.log(num);
