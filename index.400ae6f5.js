f0ee97c895b8752ce2c23cf6b114475094390450
hvoarang <90283144+hvoarang@users.noreply.github.com>
2023-03-06 00:20:15 +0000

    Deploying to gh-pages from @ hvoarang/js-only@1b5486f878e238f16b692a12ba040d5323026f11 🚀

--- index.400ae6f5.js
+++
@@ -1,2 +0,0 @@
-const o=["Mango","Poly","Ajax"];console.log(o[0]),console.log(o[1]),console.log(o.length);const l=o.length-1;console.log(o[l]);
-//# sourceMappingURL=index.400ae6f5.js.map
---
+++ index.d66e5aac.js
@@ -0,0 +1,2 @@
+!function(){var o=["Mango","Poly","Ajax"];console.log(o[0]),console.log(o[1]),console.log(o.length);var l=o.length-1;console.log(o[l]),console.log(o.push());for(var n=[1,2,3],g=[],e=0;e<n.length;e+=1)console.log(n[e]),g=n[e].push;console.log(g)}();
+//# sourceMappingURL=index.d66e5aac.js.map
