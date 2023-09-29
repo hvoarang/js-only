293b57da92cf89862392d25ffbe161c463090025
hvoarang <hvoarang@gmail.com>
2023-09-29 22:52:33 +0000

    Deploying to gh-pages from @ hvoarang/js-only@70373842b743258092f6b22bd59c8e8a855a906b 🚀

---
+++ index.dda44da6.js
@@ -0,0 +1,2 @@
+console.log("red".length);const o=["Mango","Poly","Ajax"];console.log(o[0]),console.log(o[1]),console.log(o.length);const l=o.length-1;console.log(o[l]),console.log(o.push());const n=[1,2,3];let e=[];for(let o=0;o<n.length;o+=1)console.log(n[o]),e=n[o].push;console.log(n[i]);
+//# sourceMappingURL=index.dda44da6.js.map
