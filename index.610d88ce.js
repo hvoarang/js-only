8dfea28ccec011cd5510e6e8f95d5bba0a63ef94
hvoarang <hvoarang@gmail.com>
2023-10-03 23:29:04 +0000

    Deploying to gh-pages from @ hvoarang/js-only@3e2c0eb1aaf6c5c11199346dfeab4b16a586713a 🚀

---
+++ index.610d88ce.js
@@ -0,0 +1,2 @@
+!function(){asd;var o=["Mango","Poly","Ajax"];console.log(o[0]),console.log(o[1]),console.log(o.length);var l=o.length-1;console.log(o[l]),console.log(o.push());for(var n=[1,2,3],g=0;g<n.length;g+=1)console.log(n[g]),n[g].push;console.log(n[i])}();
+//# sourceMappingURL=index.610d88ce.js.map
