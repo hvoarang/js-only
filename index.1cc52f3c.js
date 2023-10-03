94b6d4d9df72def4dc49d58fc947fa07a2debe6f
hvoarang <hvoarang@gmail.com>
2023-10-03 23:28:01 +0000

    Deploying to gh-pages from @ hvoarang/js-only@92a32ab58bf57088fb4020bd03af80519f9c893f 🚀

---
+++ index.1cc52f3c.js
@@ -0,0 +1,2 @@
+!function(){new Object;var o={color:"",weight:"",price:Number};console.log(o.price);var l=["Mango","Poly","Ajax"];console.log(l[0]),console.log(l[1]),console.log(l.length);var e=l.length-1;console.log(l[e]),console.log(l.push());for(var n=[1,2,3],c=0;c<n.length;c+=1)console.log(n[c]),n[c].push;console.log(n[i])}();
+//# sourceMappingURL=index.1cc52f3c.js.map
---
+++ index.6d75e85d.js
@@ -0,0 +1,2 @@
+new Object;let o={color:"",weight:"",price:Number};console.log(o.price);const l=["Mango","Poly","Ajax"];console.log(l[0]),console.log(l[1]),console.log(l.length);const e=l.length-1;console.log(l[e]),console.log(l.push());const n=[1,2,3];let c=[];for(let o=0;o<n.length;o+=1)console.log(n[o]),c=n[o].push;console.log(n[i]);
+//# sourceMappingURL=index.6d75e85d.js.map
