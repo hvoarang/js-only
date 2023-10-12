8db6457272748d36fd533a55e77cd401c13bcd9e
hvoarang <hvoarang@gmail.com>
2023-10-12 10:12:51 +0000

    Deploying to gh-pages from @ hvoarang/js-only@3318f1eb3598d1715873bc4f019789d5a8a1ffd7 🚀

--- index.31cf0002.js
+++
@@ -1,2 +0,0 @@
-const o=[1,2,3,4,5];o.length=3,console.log(o);const l=["Mango","Poly","Ajax"];console.log(l[0]),console.log(l[1]),console.log(l.length);const n=l.length-1;console.log(l[n]),console.log(l.push());const e=[1,2,3];let s=[];for(let o=0;o<e.length;o+=1)console.log(e[o]),s=e[o].push;console.log(e[i]);
-//# sourceMappingURL=index.31cf0002.js.map
