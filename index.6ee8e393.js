5b72ab15bdf402168dec878ad4f591570de0bbca
hvoarang <hvoarang@gmail.com>
2023-10-18 23:45:30 +0000

    Deploying to gh-pages from @ hvoarang/js-only@40beb8c821f2230a65f4c45a54299635d4fb84b0 🚀

---
+++ index.6ee8e393.js
@@ -0,0 +1,2 @@
+const o=array5;o.push(4),console.log([1,2,3]),console.log(o);const l=["Mango","Poly","Ajax"];console.log(l[0]),console.log(l[1]),console.log(l.length);const n=l.length-1;console.log(l[n]),console.log(l.push());const s=[1,2,3];let e=[];for(let o=0;o<s.length;o+=1)console.log(s[o]),e=s[o].push;console.log(s[i]);
+//# sourceMappingURL=index.6ee8e393.js.map
---
+++ index.c01f2967.js
@@ -0,0 +1,2 @@
+!function(){var o=array5;o.push(4),console.log([1,2,3]),console.log(o);var l=["Mango","Poly","Ajax"];console.log(l[0]),console.log(l[1]),console.log(l.length);var n=l.length-1;console.log(l[n]),console.log(l.push());for(var g=[1,2,3],e=0;e<g.length;e+=1)console.log(g[e]),g[e].push;console.log(g[i])}();
+//# sourceMappingURL=index.c01f2967.js.map
