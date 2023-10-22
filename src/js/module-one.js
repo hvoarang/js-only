f9fca802434d9a8e0f909b5fd540fbbf6a45432b
Denys iElkin <hvoarang@gmail.com>
2023-10-23 00:44:01 +0300

    asd

--- src/js/module-one.js
+++ src/js/module-one.js
@@ -1,23 +1,17 @@
 // итерация массива
 const clients = ['Mango', 'Poly', 'Ajax'];
-//.. итерация начинается с нуля
+// итерация начинается с нуля
 console.log(clients[0]);
 console.log(clients[1]);
-//
-//
+
 // длина массива
 console.log(clients.length);
-//
-//
+
 // индекс последнего массива
 const lastElementIndex = clients.length - 1;
 console.log(clients[lastElementIndex]); //Ajax
 console.log(clients.push());
 
-const arr = [1, 2, 3];
-let num = [];
-for (let i = 0; i < arr.length; i += 1) {
-  console.log(arr[i]);
-  num = arr[i].push;
-}
-console.log(arr[i]);
+const arr = [1, 2, 3, 4, 5];
+
+let sumArr = [];
