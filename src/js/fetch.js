7f4f8ba2aaa5b372e42addff1630520aef46d6cd
Denys iElkin <hvoarang@gmail.com>
2023-11-06 11:50:26 +0200

    asd

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -1,9 +1,13 @@
-const array1 = [1, 2, 3];
-const array2 = array1;
+// const array1 = [1, 2, 3];
+// const array2 = array1;
 
-array2.push(4);
+// array2.push(4);
 
-console.log(`${array1} - massive one`);
-console.log(`${array2} - massive two`);
+// console.log(`${array1} - massive one`);
+// console.log(`${array2} - massive two`);
 
-console.log(array1.length - 2);
+// console.log(array1.slice(3));
+
+const arr = [11, 2, 3, 4, 5, 6];
+arr.slice(0, 1);
+console.log(arr);
