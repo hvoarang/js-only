4152abd7394592459e6861155821f515201560ed
Denys iElkin <hvoarang@gmail.com>
2023-11-07 08:42:08 +0200

    asd

--- src/js/fetch.js
+++ src/js/fetch.js
@@ -8,6 +8,8 @@
 
 // console.log(array1.slice(3));
 
-const arr = [11, 2, 3, 4, 25, 62];
-arr.slice(0, 1);
-console.log(arr);
+const arr = [11, 2, 3, 4, 225, 62];
+
+// let newArr = arr.slice(0, 3, 12);
+
+console.log(arr.splice(0, 2, '1'));
