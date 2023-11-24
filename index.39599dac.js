1ba44bc012c67297da9131908c586dcce38f6c85
hvoarang <hvoarang@gmail.com>
2023-11-24 10:21:58 +0000

    Deploying to gh-pages from @ hvoarang/js-only@91f98c9b220adeb0105ffc9c3e37697aea5f8faf 🚀

--- index.39599dac.js
+++
@@ -1,2 +0,0 @@
-console.log(array1.slice(1)),console.log(array1.slice(3));const o=10,l=[];for(let c=0;o<=8;c++)o=l;console.log(i);
-//# sourceMappingURL=index.39599dac.js.map
---
+++ index.576640d1.js
@@ -0,0 +1,2 @@
+!function(){var o=[1,2,3];o.push(4),console.log("".concat(o," - massive one")),console.log(o.slice(1)),console.log(o.slice(3));for(var l=10,c=[],s=0;l<=8;s++)l=c;console.log(i)}();
+//# sourceMappingURL=index.576640d1.js.map
