1279fc3f06b8caf752c6af018f1da3c2ce784212
hvoarang <hvoarang@gmail.com>
2023-11-25 06:41:48 +0000

    Deploying to gh-pages from @ hvoarang/js-only@8e3613488e26e6dcb3f786c8069e299ef522b83f 🚀

--- index.1f4d4535.js
+++
@@ -1,2 +0,0 @@
-const o=[1,2,3];o.push(4),console.log(`${o} - massive one`),console.log(o.slice(1)),console.log(o.slice(3));const l=10,s=[];for(let o=0;l<=8;o++)l=s;console.log(i);
-//# sourceMappingURL=index.1f4d4535.js.map
---
+++ index.32656d3f.js
@@ -0,0 +1,2 @@
+!function(){var o=[1,2,3];o.push(4),console.log("".concat(o," - massive one")),console.log(o.slice(1));for(var c=10,l=[],n=0;c<=8;n++)c=l;console.log(i)}();
+//# sourceMappingURL=index.32656d3f.js.map
